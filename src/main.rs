b00b779ac97317ddc386663c5dafdb004952b1b0
Roy Brunton <roy@flashtalking.com>
2016-09-19 15:14:38 +0100

    moved parser code to own module

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     println!("{:?}", parser::parse_Block(r#"
     for (enum, iter, 0..100:10) { #true };
     for (iter, 0..100:10) { #true };
-    for (0..100) { #true };
+    for (0..100) { #true }
     "#).unwrap());
 }
 
